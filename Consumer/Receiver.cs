537706e698ac4b7deba96792f9a8908057079a0d
juanarayafonsec <juan-jose95@hotmail.com>
2023-05-16 00:59:25 +0200

    Initial commit

---
+++ Consumer/Receiver.cs
@@ -0,0 +1,26 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+
+var factory = new ConnectionFactory() { HostName = "localhost" };
+using (var connection = factory.CreateConnection())
+using (var channel = connection.CreateModel())
+{
+    channel.QueueDeclare("BasicTest", false, false, false, null);
+
+    var consumer = new EventingBasicConsumer(channel);
+
+
+    consumer.Received += (model, ea) =>
+    {
+        var body = ea.Body;
+        string message = Encoding.UTF8.GetString(body.Span);
+        Console.WriteLine("Received message {0}...", message);
+    };
+
+    channel.BasicConsume("BasicTest", true, consumer);
+
+    Console.WriteLine("Press [enter] tp exit the Consumer...");
+
+    Console.ReadLine();
+}
